04ab0567be78d1f2ec908787ddb5128df469f04e
Jori Huisman <jori@huisman.io>
2026-04-19 23:05:43 +0200

    SprayPaintPlus v1.2.5: repoint repo URLs to StationeersPlus monorepo

--- Mods/SprayPaintPlus/SprayPaintPlus/Plugin.cs
+++ Mods/SprayPaintPlus/SprayPaintPlus/Plugin.cs
@@ -20,7 +20,7 @@ namespace SprayPaintPlus
     {
         public const string PluginGuid = "net.spraypaintplus";
         public const string PluginName = "SprayPaintPlus";
-        public const string PluginVersion = "1.2.4";
+        public const string PluginVersion = "1.2.5";
 
         internal static readonly Mod MOD = new Mod(PluginName, PluginVersion);
 
